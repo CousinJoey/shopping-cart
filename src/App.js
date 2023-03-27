eaae3e4c5d9ec525b0a2be41127311ca0710b8b6
CousinJoey <cousinjoeyllc@gmail.com>
2023-03-27 14:44:10 -0400

    foundation files + linter/prettier

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Main from "./componets/main";
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+      <p>Hello world</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componets/main.js
@@ -0,0 +1,18 @@
+import "../style.css";
+import React, { Component } from "react";
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <p id="main-text">This is main</p>
+      </div>
+    );
+  }
+}
+
+export default Main;
